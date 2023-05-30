8c37cad01fcd2bc2f29e88881116fe1106ea7e42
Alec Hannaford <alec322@gmail.com>
2023-05-30 16:12:49 -0700

    date range endpoint functional

--- Controllers/dateRange.js
+++ Controllers/dateRange.js
@@ -7,4 +7,17 @@ router.get("/", (req,res) => {
     res.json('GET ROUTE XD!!!')
 })
 
+//date range endpoint
+router.get('/:start/:end', async (req,res) => {
+    try{
+        res.json(await dateReport.find(
+            {reportDate: {$gte: req.params.start , $lte: req.params.end}}
+            )
+        )
+    } catch(error){
+        res.status(400).json(error)
+    }
+
+})
+
 module.exports = router;
